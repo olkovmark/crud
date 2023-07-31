6393152413f4ea6edca1721831d59936964b1f97
olkovmark <olkovmark@gmail.com>
2023-07-31 12:46:43 +0200

    user-1

--- src/route/index.js
+++ src/route/index.js
@@ -1,25 +1,87 @@
-// Підключаємо технологію express для back-end сервера
 const express = require('express')
-// Cтворюємо роутер - місце, куди ми підключаємо ендпоїнти
+
 const router = express.Router()
 
-// ================================================================
+// ===============================
+class User {
+  static #list = []
+  constructor(email, login, password) {
+    this.email = email
+    this.login = login
+    this.password = password
+    this.id = new Date().getTime()
+  }
 
-// router.get Створює нам один ентпоїнт
+  static add = (user) => {
+    this.#list.push(user)
+  }
 
-// ↙️ тут вводимо шлях (PATH) до сторінки
+  static getList = () => this.#list
+
+  static getById = (id) => {
+    return this.#list.find((user) => user.id === id)
+  }
+
+  static delete = (id) => {
+    this.#list = this.#list.filter((user) => user.id !== id)
+    return true
+  }
+
+  static updateEmail(id, newEmail, password) {
+    const user = this.#list.find((user) => user.id === id)
+    if (!user) return 'User not found'
+
+    if (user.password !== password)
+      return 'Password is incorrect'
+
+    user.email = newEmail
+    return 'Email update'
+  }
+}
+
+// ===============================
+
 router.get('/', function (req, res) {
-  // res.render генерує нам HTML сторінку
-
-  // ↙️ cюди вводимо назву файлу з сontainer
+  User.add(new User('123@123', '123', '123'))
+  const list = User.getList()
   res.render('index', {
-    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
     style: 'index',
+    data: {
+      users: {
+        list,
+        isEmpty: list.length === 0,
+      },
+    },
   })
-  // ↑↑ сюди вводимо JSON дані
+})
+router.post('/user-create', function (req, res) {
+  const { email, login, password } = req.body
+  const user = new User(email, login, password)
+
+  User.add(user)
+
+  res.render('success-info', {
+    style: 'success-info',
+    info: 'User is created',
+  })
 })
 
-// ================================================================
+router.post('/update-email', function (req, res) {
+  const { id, email, password } = req.body
+  console.log(req.body)
 
-// Підключаємо роутер до бек-енду
+  const response = User.updateEmail(
+    Number(id),
+    email,
+    password,
+  )
+
+  console.log(response)
+
+  res.render('success-info', {
+    style: 'success-info',
+    info: response,
+  })
+})
+
 module.exports = router
