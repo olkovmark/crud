1c805679a172d2f4551666bbc6d45af9d85e5daa
olkovmark <olkovmark@gmail.com>
2023-08-01 11:33:46 +0200

    ;2Dfirst

--- src/route/index.js
+++ src/route/index.js
@@ -1,25 +1,22 @@
-// Підключаємо технологію express для back-end сервера
 const express = require('express')
-// Cтворюємо роутер - місце, куди ми підключаємо ендпоїнти
+
 const router = express.Router()
 
-// ================================================================
-
-// router.get Створює нам один ентпоїнт
-
-// ↙️ тут вводимо шлях (PATH) до сторінки
 router.get('/', function (req, res) {
-  // res.render генерує нам HTML сторінку
-
-  // ↙️ cюди вводимо назву файлу з сontainer
   res.render('index', {
-    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
     style: 'index',
   })
-  // ↑↑ сюди вводимо JSON дані
+})
+router.get('/product-create', function (req, res) {
+  res.render('product-create', {
+    style: 'index',
+  })
 })
 
-// ================================================================
+router.post('/product-create', function (req, res) {
+  res.render('alert', {
+    style: 'index',
+  })
+})
 
-// Підключаємо роутер до бек-енду
 module.exports = router
